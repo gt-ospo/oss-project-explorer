825de53486c1cf832335bd9f81657804b85d9b83
Jeffrey Young <jyoung3131@users.noreply.github.com>
2024-09-30 10:13:43 -0400

    Update AboutSection.jsx
    Some minor text fixes and a pointer to the repository.

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -4,8 +4,9 @@ const AboutSection = () => {
     return (
         <div className="mx-auto max-w-5xl py-4 px-6 mt-4 mb-2 bg-gray-100 rounded-lg shadow-md">
             <h2 className="text-xl font-semibold mb-2">About This Resource</h2>
-            <p>This page provides an overview of Georgia Tech open source research projects curated by the Georgia Tech Open Source Project Office (OSPO). Georgia Tech PIs and researchers can submit their work via the form below, and all submissions are reviewed via a PR process before being posted.</p>
-            <p>If you have any questions about this resource, please reach out to us via email at <a href="mailto:ospo-directors@gatech.edu" className="text-gtgold hover:text-gtgoldlight">ospo-directors@gatech.edu</a>.</p>
+            <p>This page provides an overview of Georgia Tech open source research projects curated by the Georgia Tech Open Source Program Office (OSPO). Georgia Tech PIs and researchers can submit their work via the Submit New Project form below, and all submissions are reviewed via a PR process before being posted.</p>
+            <br>
+            <p>If you have any questions about this resource, please reach out to us via email at <a href="mailto:ospo-directors@gatech.edu" className="text-gtgold hover:text-gtgoldlight">ospo-directors@gatech.edu</a>. If you would like to use this project for your own OSPO or OSS effort, please see the main repository at <a href="https://github.com/gt-ospo/oss-project-explorer" className="text-gtgold hover:text-gtgoldlight">our GitHub site.</p>
         </div>
     )
 }
